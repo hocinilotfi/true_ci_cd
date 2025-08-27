e78582bb2b801c782691e98c3e3091a05e905922
hocinilotfi <lotfi.hocini@hotmail.com>
2025-08-27 16:22:56 +0200

    another enpoint2

--- app/main.py
+++ app/main.py
@@ -12,3 +12,8 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: int):
+    return {"user_id": user_id}
