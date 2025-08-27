b5d79b3ccdad4bc00865f9ea23b577e24ceebdce
hocinilotfi <lotfi.hocini@hotmail.com>
2025-08-27 16:19:19 +0200

    another enpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+
 app = FastAPI()
 
-@app.get('/')
+
+@app.get("/")
 async def root():
-    return {'message': 'Hello World'}
+    return {"message": "Hello World"}
+
+
+# an additional comment
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
