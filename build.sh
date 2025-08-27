dd1611955127d0ea60d7b82dbdaec8e4a3696f87
hocinilotfi <lotfi.hocini@hotmail.com>
2025-08-27 15:47:38 +0200

    first commit fix build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # build the image the docker api_for_ci image to docker hub
 docker build -t api_for_cicd .
 docker tag api_for_cicd lotfihocini/api_for_cicd:latest
-docker push lotfijeddi/api_for_cicd:latest
+docker push lotfihocini/api_for_cicd:latest
 
