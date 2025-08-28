771757017b23d2bd81fd2821e381a94c15b04a0f
hocinilotfi <lotfi.hocini@hotmail.com>
2025-08-28 16:04:26 +0200

    add welcome message

--- app/main.py
+++ app/main.py
@@ -8,17 +8,6 @@ async def root():
     return {"message": "Hello World"}
 
 
-# an additional comment
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
-@app.get("/users/{user_id}")
-async def read_user(user_id: int):
-    return {"user_id": user_id}
-
-
 # add welcome endpoint
 @app.get("/welcome")
 async def welcome():
